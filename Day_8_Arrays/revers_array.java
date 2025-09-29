b90e22bbbffaab9d79bf97026c8adb24e1177e00
nihalmore05 <nihalmore05@gmail.com>
2025-09-29 17:46:53 +0530

    Day 8 array part Q.5

---
+++ Day_8_Arrays/revers_array.java
@@ -0,0 +1,23 @@
+package Day_8_Arrays;
+
+public class revers_array {
+    public static void main(String[] args) {
+
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        int n = numbers.length;
+
+        // this loop for using normal array printing
+
+        System.out.print("The Normal array is : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        // this loop for using reverse array
+        System.out.print("\nRevers array is :");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+
+        }
+    }
+}
